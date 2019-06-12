0e6491278b1152fa5a24fe656715724f49e6c275
KimSeokRae <suklea@naver.com>
2019-06-12 10:10:44 +0900

    test

--- src/main/java/com/care/board_dao/BoardDAO.java
+++ src/main/java/com/care/board_dao/BoardDAO.java
@@ -164,8 +164,24 @@ public class BoardDAO {
 		return result;
 		
 	}
+
+
 	
 	//삭제
+	public int delete(int num) {
+		String sql = "delete from final_board where num=?";
+		
+		try {
+			con = DriverManager.getConnection(url,user,pwd);
+			ps = con.prepareStatement(sql);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return 0;
+	}
 	
 	//검색
 	
